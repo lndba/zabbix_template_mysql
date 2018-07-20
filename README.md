060028f0d60218c01bd2eb4c25fcd6d24bed6252
wang_zhw <wang_zhw@myepay.cn>
2018-07-20 21:26:05 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
-# zabbix_template_mysql
-zabbix监控MySQL模版
+# Zabbix之MySQL监控模板详解
+## zabbix agent配置
+1. 上传监控MySQL脚本和更改check_list.txt
+
+```
+[root@linux-node1 ~]# ll /server/scripts/zabbix_monitor/monitor_mysql.sh
+-rwxr-xr-x 1 root root 4020 Aug 16 10:49 /server/scripts/zabbix_monitor/monitor_mysql.sh
+[root@linux-node1 ~]# chmod +x /server/scripts/zabbix_monitor/monitor_mysql.sh
+[root@linux-node1 ~]# chown zabbix.zabbix /server/scripts/zabbix_monitor/check_list.txt
+[root@linux-node1 ~]# chmod 600 /server/scripts/zabbix_monitor/check_list.txt
+```
+
+2. 将自定义监控文件monitor.conf文件上传到/etc/zabbix/zabbix_agentd.d下
+
+```
+[root@linux-node1]# cat /etc/zabbix/zabbix_agentd.d/monitor.conf
+# MySQL Status
+UserParameter=mysql.discovery,/server/scripts/zabbix_monitor/monitor_mysql.sh discovery
+UserParameter=mysql.slave.discovery,sh /server/scripts/zabbix_monitor/monitor_mysql.sh slave_discovery
+UserParameter=mysql.ping[*],sh /server/scripts/zabbix_monitor/monitor_mysql.sh ping $1
+UserParameter=mysql.slave_status[*],sh /server/scripts/zabbix_monitor/monitor_mysql.sh slave_status $1
+UserParameter=mysql.tmpfile.md5[*],sh /server/scripts/zabbix_monitor/monitor_mysql.sh tmpfile_md5 $1
+UserParameter=mysql.perf[*],sh /server/scripts/zabbix_monitor/monitor_mysql.sh perf $2 $1
+```
+
+3. zabbix agent配置文件开启agent主动模式
+
+```
+[root@linux-node1 ~]# vim /etc/zabbix/zabbix_agentd.conf
+ServerActive=192.168.56.11
+```
+
+4. 配置MySQL信息
+
+```
+[root@linux-node1 ~]# cat /server/scripts/zabbix_monitor/check_list.txt
+mysql:  3306    slave  zabbix zabbix123.Com
+proc:   mysqld
+```
+
+5. check_list.txt文件配置MySQL信息详解
+
+第一行：服务名，端口号，主从，MySQL用户名，MySQL密码
+第二行：进程，进程名
+温馨提示：数据库账号授权方法：
+GRANT USAGE,PROCESS,REPLICATION CLIENT,REPLICATION SLAVE ON *.* TO 'zabbix'@'localhost' IDENTIFIED BY 'zabbix123.Com';
+flush privileges;
+
+##Zabbix界面配置
+1. 导入模版
+![](media/15320914268637/15320923293018.jpg)
+
+2. 选择导入摸板
+![](media/15320914268637/15320924407503.jpg)
+
+3. 添加自动发现所需的正则表达式
+![](media/15320914268637/15320925021953.jpg)
+
+4. 新建表达式
+![](media/15320914268637/15320925701131.jpg)
+
+5. 添加正则表达式完成
+![](media/15320914268637/15320926427916.jpg)
+
+6. 到此为止，MySQL监控模板就可以正常使用了
+
---
+++ Scripts/monitor_mysql.sh
@@ -0,0 +1,162 @@
+#!/bin/bash
+#===================Script Description===================
+#Scripts_name     : monitor_mysql.sh
+#Scripts_function : This script applies to MySQL monitoring based on the ZABBIX template.
+#Author           : 行思(Lionel Wang)
+#E_mail           : zhiwei_wang@163.com
+#QQ               : 805022948
+#Blog             : www.lndba.com
+#Create date      : 2016-08-15
+#========================================================
+. /etc/profile
+. /etc/bashrc
+
+mysql_cli=$(which mysql 2> /dev/null)
+mysql_cli=${mysql_cli:=/application/mysql/bin/mysql}
+mysql_cli=${mysql_cli:=/usrl/local/mysql/bin/mysql}
+cwdir=`dirname $0`
+check_list=${cwdir}/check_list.txt
+
+if [ ! -z "$2" ];then
+    port=$2
+else
+    port=3306
+fi
+
+
+str_md5() {
+    echo $1 | md5sum | awk '{ print $1 }'
+}
+
+
+json_null() {
+    printf '{\n'
+    printf '\t"data":[\n'
+    printf  "\t\t{ \n"
+    printf  "\t\t\t\"{#PORT}\":\"NULL\"}]}\n"
+}
+
+tmpfile=/tmp/.$(str_md5 ${port}.mysql).zbx
+
+if [ -f $check_list ];then
+    user=$(grep $port $check_list | awk '{ print $4 }')
+    password=$(grep $port $check_list | awk '{ print $5 }')
+fi
+
+user=${user:=lionel}
+password=${password:=123456}
+host=${host:=localhost}
+mysql_connect="${mysql_cli} -u$user -p$password -h$host -P$port"
+
+mysql_discovery() {
+    if [ ! -f "$check_list" ];then
+        json_null
+        exit 2
+    fi
+    ports=($(grep -v '^#' $check_list | grep '^mysql:' | awk '{ print $2 }'))
+    if [ ${#ports[@]} -eq 0 ];then
+        json_null
+        exit 1
+    fi
+    printf '{\n'
+    printf '\t"data":[\n'
+    for((i=0;i<${#ports[@]};++i)) {
+        num=$(echo $((${#ports[@]}-1)))
+        if [ "$i" != "${num}" ]; then
+            printf "\t\t{ \n"
+            printf "\t\t\t\"{#PORT}\":\"${ports[$i]}\"},\n"
+        else
+            printf  "\t\t{ \n"
+            printf  "\t\t\t\"{#PORT}\":\"${ports[$num]}\"}]}\n"
+        fi
+    }
+}
+
+slave_discovery() {
+    if [ ! -f "$check_list" ];then
+        json_null
+        exit 2
+    fi
+    ports=($(grep -v '^#' $check_list| grep "^mysql:" | grep 'slave' | awk '{ print $2 }'))
+    if [ ${#ports[@]} -eq 0 ];then
+        json_null
+        exit 1
+    fi
+    printf '{\n'
+    printf '\t"data":[\n'
+    for((i=0;i<${#ports[@]};++i)) {
+        num=$(echo $((${#ports[@]}-1)))
+        if [ "$i" != "${num}" ]; then
+            printf "\t\t{ \n"
+            printf "\t\t\t\"{#PORT}\":\"${ports[$i]}\"},\n"
+        else
+            printf  "\t\t{ \n"
+            printf  "\t\t\t\"{#PORT}\":\"${ports[$num]}\"}]}\n"
+        fi
+    }
+}
+
+mysql_ping() {
+        $mysql_connect -e 'show global status' 2> /dev/null > $tmpfile && $mysql_connect -e 'show global variables' 2> /dev/null >> $tmpfile && $mysql_connect -e 'show slave status\G' 2> /dev/null  >> $tmpfile && echo 1 || echo 0
+}
+
+mysql_perf() {
+    data=$(grep "\<$1\>" $tmpfile | awk '{ print $2 }')
+    if [ -n "$data" ];then
+        echo $data
+    fi
+}
+
+slave_status() {
+        slave_running=`mysql_perf 'Slave_IO_State'`
+        io_running=`mysql_perf 'Slave_IO_Running'` 
+        sql_running=`mysql_perf 'Slave_SQL_Running'`
+        if [ "$slave_running" != '' ];then
+            if [ "$io_running" == 'No' -o "$sql_running" == 'No' ];then
+                echo 0
+            else
+                echo 1
+            fi
+        else
+            if [ "$io_running" == 'Yes' -a "$sql_running" == 'Yes' ];then
+               echo 1
+            else
+               echo 3
+            fi
+        fi
+        
+}
+
+
+tmpfile_md5() {
+    /usr/bin/md5sum $tmpfile 2> /dev/null | awk '{ print $1 }' || echo "7410"
+}
+
+case $1 in
+    discovery)
+        mysql_discovery
+        ;;
+    slave_discovery)
+        slave_discovery
+        ;;
+    ping)
+        mysql_ping
+        ;;
+    slave_status)
+        slave_status
+        ;;
+    tmpfile_md5)
+        tmpfile_md5
+        ;;
+    perf)
+        if [ -z "$3" ];then
+            echo 7410
+        else
+            mysql_perf $3
+        fi
+        ;;
+    *)
+        usage="Usage: $0 discovery | slave_discovery | ping | slave_status | tmpfile_md5 | perf  port [options]"
+        echo $usage
+        ;;
+esac
